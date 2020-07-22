f2b9302c5e5e0ceea9f967475a92965f773cff50
bitman11 <bitman@hellobitman.com>
2020-07-22 16:59:45 +0900

    ggg

--- dist/get-bitcoin-key-pair.js
+++ dist/get-bitcoin-key-pair.js
@@ -18,43 +18,53 @@ var bs58 = require('bs58');
 
 var RIPEMD160 = require('ripemd160');
 
-var SecureRandom = require('secure-random'); //n-1.1578*10**77
+var secureRandom = require('secure-random');
+
+var crypto = require('crypto'); //n-1.1578*10**77
 //console.log(msg);
 
 
-var getBitcoinAddress = function getBitcoinAddress(_randomString) {
-  var addrVer = Buffer.alloc(1, 0x00);
-  var wifByte = Buffer.alloc(1, 0x80);
-  var randomHexFromRandomString = sha256(_randomString);
-  var randomByteFromRandomHex = Buffer.from(randomHexFromRandomString, 'hex');
+function hasha256(data) {
+  return crypto.createHash('sha256').update(data).digest();
+}
 
-  if (_randomString && secp256k1.privateKeyVerify(randomByteFromRandomHex)) {
-    var publicKey = secp256k1.publicKeyCreate(randomByteFromRandomHex);
-    var wifBufPriv = Buffer.concat([wifByte, randomByteFromRandomHex], wifByte.length + randomByteFromRandomHex.length);
-    var wifHashFirst = sha256(wifBufPriv);
-    var wifHashSecond = Buffer.from(sha256(wifHashFirst), 'hex');
-    var wifHashSig = wifHashSecond.slice(0, 4);
-    var wifBuf = Buffer.concat([wifBufPriv, wifHashSig], wifBufPriv.length + wifHashSig.length);
-    var wifFinal = bs58.encode(wifBuf);
-    var publicKeyInitialHash = sha256(Buffer.from(publicKey, 'hex'));
-    var publicKeyRIPEHash = new RIPEMD160().update(Buffer.from(publicKeyInitialHash, 'hex')).digest('hex');
-    var hashBuffer = Buffer.from(publicKeyRIPEHash, 'hex');
-    var concatHash = Buffer.concat([addrVer, hashBuffer], addrVer.length + hashBuffer.length);
-    var hashExtRipe = Buffer.from(sha256(concatHash), 'hex');
-    var hashExtRipe2 = Buffer.from(sha256(hashExtRipe), 'hex');
-    var hashSig = hashExtRipe2.slice(0, 4);
-    var bitcoinBinaryStr = Buffer.concat([concatHash, hashSig], concatHash.length + hashSig.length);
-    var bitcoinAddress = bs58.encode(Buffer.from(bitcoinBinaryStr));
-    return {
-      bitcoinAddress: bitcoinAddress,
-      bitCoinPrivateKey: wifFinal
-    };
-  } else {
-    console.log('The string cannot be used. Try other string');
-    throw {
-      error: 'The string cannot be used as key. Try others'
-    };
+var getBitcoinAddress = function getBitcoinAddress(_randomString) {
+  var verByte = Buffer.alloc(1, 0x00);
+  var wifByte = Buffer.alloc(1, 0x80);
+  var compByte = Buffer.alloc(1, 0x01);
+  var randomHexFromRandomString = hasha256(_randomString);
+  var randomByteFromRandomHex = Buffer.from(randomHexFromRandomString, 'hex');
+  console.log(randomHexFromRandomString);
+  var checksum = hasha256(randomHexFromRandomString).slice(0, 8);
+  console.log('checksum', checksum);
+  var checkSumByte = Buffer.from(checksum, 'hex'); //let randomByteFromRandomHex = secureRandom.randomBuffer(32);
+
+  if (!secp256k1.privateKeyVerify(randomByteFromRandomHex)) {
+    throw 'the provided string cannot be private key';
   }
+
+  var wifBufPriv = Buffer.concat([wifByte, randomByteFromRandomHex, compByte, checkSumByte], wifByte.length + randomByteFromRandomHex.length + compByte.length, checkSumByte.length);
+  var wifHashFirst = hasha256(wifBufPriv);
+  var wifHashSecond = Buffer.from(hasha256(wifHashFirst), 'hex');
+  var wifHashSig = wifHashSecond.slice(0, 4);
+  var wifBuf = Buffer.concat([wifBufPriv, wifHashSig], wifBufPriv.length + wifHashSig.length);
+  var wifFinal = bs58.encode(wifBuf);
+  var publicKey = secp256k1.publicKeyCreate(randomByteFromRandomHex);
+  var publicKeyInitialHash = hasha256(Buffer.from(publicKey, 'hex'));
+  var publicKeyRIPEHash = new RIPEMD160().update(Buffer.from(publicKeyInitialHash, 'hex')).digest('hex');
+  var hashBuffer = Buffer.from(publicKeyRIPEHash, 'hex');
+  var concatHash = Buffer.concat([verByte, hashBuffer], verByte.length + hashBuffer.length);
+  var hashExtRipe = Buffer.from(hasha256(concatHash), 'hex');
+  var hashExtRipe2 = Buffer.from(hasha256(hashExtRipe), 'hex');
+  var hashSig = hashExtRipe2.slice(0, 4);
+  var bitcoinBinaryStr = Buffer.concat([concatHash, hashSig], concatHash.length + hashSig.length);
+  var bitcoinAddress = bs58.encode(Buffer.from(bitcoinBinaryStr));
+  console.log(wifFinal.length);
+  console.log(bitcoinAddress.length);
+  return {
+    bitcoinAddress: bitcoinAddress,
+    bitCoinPrivateKey: wifFinal
+  };
 };
 
 exports.getBitcoinAddress = getBitcoinAddress;
--- src/lib/get-bitcoin-key-pair.js
+++ src/lib/get-bitcoin-key-pair.js
@@ -4,38 +4,53 @@ const sha256 = require('sha256');
 const ByteBuffer = require('bytebuffer');
 const bs58 = require('bs58');
 const RIPEMD160 = require('ripemd160');
-const SecureRandom = require('secure-random');
-
+const secureRandom = require('secure-random');
+const crypto = require('crypto')
 //n-1.1578*10**77
 //console.log(msg);
 
-export const getBitcoinAddress = (_randomString) => {
-    let addrVer = Buffer.alloc(1, 0x00);
-    let wifByte = Buffer.alloc(1, 0x80);
-    let randomHexFromRandomString = sha256(_randomString)
-    let randomByteFromRandomHex = Buffer.from(randomHexFromRandomString, 'hex');
-    if(_randomString && secp256k1.privateKeyVerify(randomByteFromRandomHex)){
-        let publicKey = secp256k1.publicKeyCreate(randomByteFromRandomHex);
-        let wifBufPriv = Buffer.concat([wifByte, randomByteFromRandomHex], wifByte.length+randomByteFromRandomHex.length);
-        let wifHashFirst = sha256(wifBufPriv);
-        let wifHashSecond = Buffer.from(sha256(wifHashFirst), 'hex');
-        let wifHashSig = wifHashSecond.slice(0,4);
-        let wifBuf = Buffer.concat([wifBufPriv, wifHashSig], wifBufPriv.length+wifHashSig.length);
-        let wifFinal = bs58.encode(wifBuf);
-        let publicKeyInitialHash = sha256(Buffer.from(publicKey, 'hex'));
-        let publicKeyRIPEHash = new RIPEMD160().update(Buffer.from(publicKeyInitialHash, 'hex')).digest('hex');
-        let hashBuffer = Buffer.from(publicKeyRIPEHash, 'hex');
+function hasha256(data) {
+    return crypto.createHash('sha256').update(data).digest();
+}
 
-        let concatHash = Buffer.concat([addrVer, hashBuffer], addrVer.length + hashBuffer.length);
-        let hashExtRipe = Buffer.from(sha256(concatHash),'hex');
-        let hashExtRipe2 = Buffer.from(sha256(hashExtRipe),'hex');
-        let hashSig = hashExtRipe2.slice(0, 4);
-        let bitcoinBinaryStr = Buffer.concat([concatHash, hashSig], concatHash.length + hashSig.length);
-        let bitcoinAddress = bs58.encode(Buffer.from(bitcoinBinaryStr));
-        return {bitcoinAddress:bitcoinAddress, bitCoinPrivateKey:wifFinal}
-    }else{
-        console.log('The string cannot be used. Try other string');
-        throw {error:'The string cannot be used as key. Try others'}
+export const getBitcoinAddress = (_randomString) => {
+    let verByte = Buffer.alloc(1, 0x00);
+    let wifByte = Buffer.alloc(1, 0x80);
+    let compByte = Buffer.alloc(1,0x01);
+    let randomHexFromRandomString = hasha256(_randomString)
+    let randomByteFromRandomHex=Buffer.from(randomHexFromRandomString,'hex');
+    console.log(randomHexFromRandomString)
+    let checksum = hasha256(randomHexFromRandomString).slice(0,8);
+    console.log('checksum', checksum)
+    let checkSumByte = Buffer.from(checksum,'hex');
+    //let randomByteFromRandomHex = secureRandom.randomBuffer(32);
+    if(!secp256k1.privateKeyVerify(randomByteFromRandomHex)){
+        throw 'the provided string cannot be private key'
     }
+
+    let wifBufPriv = Buffer.concat([wifByte, randomByteFromRandomHex, compByte, checkSumByte], wifByte.length+randomByteFromRandomHex.length+compByte.length, checkSumByte.length);
+    let wifHashFirst = hasha256(wifBufPriv);
+    let wifHashSecond = Buffer.from(hasha256(wifHashFirst), 'hex');
+    let wifHashSig = wifHashSecond.slice(0,4);
+    let wifBuf = Buffer.concat([wifBufPriv, wifHashSig], wifBufPriv.length+wifHashSig.length);
+    let wifFinal = bs58.encode(wifBuf);
+
+    let publicKey = secp256k1.publicKeyCreate(randomByteFromRandomHex);
+    let publicKeyInitialHash = hasha256(Buffer.from(publicKey, 'hex'));
+    let publicKeyRIPEHash = new RIPEMD160().update(Buffer.from(publicKeyInitialHash, 'hex')).digest('hex');
+    let hashBuffer = Buffer.from(publicKeyRIPEHash, 'hex');
+
+    let concatHash = Buffer.concat([verByte, hashBuffer], verByte.length + hashBuffer.length);
+    let hashExtRipe = Buffer.from(hasha256(concatHash),'hex');
+    let hashExtRipe2 = Buffer.from(hasha256(hashExtRipe),'hex');
+    let hashSig = hashExtRipe2.slice(0, 4);
+    let bitcoinBinaryStr = Buffer.concat([concatHash, hashSig], concatHash.length + hashSig.length);
+    let bitcoinAddress = bs58.encode(Buffer.from(bitcoinBinaryStr));
+    console.log(wifFinal.length);
+    console.log(bitcoinAddress.length);
+
+    return {bitcoinAddress:bitcoinAddress, bitCoinPrivateKey:wifFinal}
 };
+
+
 
